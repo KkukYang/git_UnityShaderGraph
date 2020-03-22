c4b74512d19ae0d9f23c96877ad41b70883d1b41
kkukyang <yangyk7364@gmail.com>
2020-03-22 23:24:35 +0900

    //11:23 branchTest1에서 추가

--- TestProject/Assets/Scripts/Test/TestScript.cs
+++ TestProject/Assets/Scripts/Test/TestScript.cs
@@ -8,7 +8,8 @@ public class TestScript : MonoBehaviour
     void Start()
     {
         //11:20 수정사항
-        //11:21 수정사항 추가
+        //11:21 수정사항 추가 브랜치 새로 딴 시점 branchTest1
+        //11:23 branchTest1에서 추가
 
 
     }
