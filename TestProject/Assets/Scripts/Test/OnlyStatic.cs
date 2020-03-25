1f1f28b7f229ee5244f520b0dcaca321d5bee407
khyang <khyang@nsstudio.co.kr>
2020-03-25 16:32:14 +0900

    add

---
+++ TestProject/Assets/Scripts/Test/OnlyStatic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnlyStatic : MonoBehaviour
+{
+    public static void TestA()
+    {
+        Debug.Log("TestA()");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
